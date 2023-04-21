733e7d2a52e75573e79ca313a4b69982303d86a3
Wadih <720013@student.dusd.net>
2023-04-21 15:03:09 -0700

    test

---
+++ Space Platformer/Assets/Kamil/Scripts/PlayerMovement.cs
@@ -0,0 +1,118 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float moveSpeed;
+    public float jumpForce;
+    public Transform ceilingCheck;
+    public Transform groundCheck;
+    public LayerMask groundObjects;
+    public float checkRadius;
+    public int maxJumpCount;
+
+    private Rigidbody2D rb;
+    private bool facingRight = true;
+    private float moveDirection;
+    private bool isJumping = false;
+    private bool isGrounded;
+    private int jumpCount;
+
+    // Awake is called after all objects are intiialized. Called in a random order.
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>(); // Will look for a component on this GameObject (what the script is attached to) of type Rigidbody2D.
+    }
+
+    private void Start()
+    {
+        jumpCount = maxJumpCount;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Get Inputs
+        ProcessInputs();
+
+        // Animate
+        Animate();
+    }
+
+    // Better for handling physics, can be called multiple times per update frame.
+    private void FixedUpdate()
+    {
+        // Check if grounded
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, checkRadius, groundObjects);
+        if (isGrounded)
+        {
+            jumpCount = maxJumpCount;
+        }
+        // Move
+        Move();
+    }
+
+    private void Move()
+    {
+        rb.velocity = new Vector2(moveDirection * moveSpeed, rb.velocity.y);
+        if (isJumping)
+        {
+            rb.AddForce(new Vector2(0f, jumpForce), ForceMode2D.Impulse);
+            jumpCount--;
+        }
+        isJumping = false;
+    }
+    private void Animate()
+    {
+        if (moveDirection > 0 && !facingRight)
+        {
+            FlipCharacter();
+        }
+        else if (moveDirection < 0 && facingRight)
+        {
+            FlipCharacter();
+        }
+    }
+    private void ProcessInputs()
+    {
+        moveDirection = Input.GetAxis("Horizontal"); // Scale of -1 -> 1.
+        if (Input.GetButtonDown("Jump") && jumpCount > 0)
+        {
+            isJumping = true;
+        }
+    }
+    private void FlipCharacter()
+    {
+        facingRight = !facingRight; // Inverse bool
+        transform.Rotate(0f, 180f, 0f);
+    }
+
+    IEnumerator PowerUpSpeed()
+    {
+        moveSpeed = 9;
+        yield return new WaitForSeconds(5);
+        moveSpeed = 5;
+    }
+
+    public void SpeedPowerUp()
+    {
+        StartCoroutine(PowerUpSpeed());
+    }
+
+    private void OnCollisionEnter2D(Collision2D col)
+    {
+        if (col.gameObject.tag == "MovingPlatform")
+        {
+            transform.parent = col.transform;
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D col)
+    {
+        if (col.gameObject.tag == "MovingPlatform")
+        {
+            transform.parent = null;
+        }
+    }
+}
