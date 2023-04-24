91553a6150320a66d2d28f5be9217e7375ade39d
Wadih <720013@student.dusd.net>
2023-04-24 15:22:22 -0700

    update

---
+++ Space Platformer/Assets/Kamil/Scripts/PlayerManager.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerManager : MonoBehaviour
+{
+    //declare two variables one max and one current health
+    public int currentHealth;
+    public int maxHealth;
+
+    PlayerMovement pMove;
+    void Awake()
+    {
+        pMove = GetComponent<PlayerMovement>();
+    }
+
+    public int coinCount;
+
+    public void Update()
+    {
+        if (currentHealth <= 0)
+        {
+            PauseGame();
+        }
+    }
+    public bool PickupItem(GameObject obj)
+    {
+        switch (obj.tag)
+        {
+            case "Currency":
+                coinCount++;
+                return true;
+            case "Speed+":
+                pMove.SpeedPowerUp();
+                return true;
+            default:
+                Debug.Log("No tag or reference is set for this GameObject");
+                return false;
+        }
+    }
+    public void TakeDamage()
+    {
+        currentHealth -= 1;
+    }
+    public void PauseGame()
+    {
+        Time.timeScale = 0;
+    }
+    public void ResumeGame()
+    {
+        Time.timeScale = 1;
+    }
+}
