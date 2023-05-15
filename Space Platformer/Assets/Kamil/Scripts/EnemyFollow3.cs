7cde03cc8f92bacc824d8078553c48c04fe86707
kamilwadih <720013@student.dusd.net>
2023-05-14 21:35:20 -0700

    update

---
+++ Space Platformer/Assets/Kamil/Scripts/EnemyFollow3.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyFollow3 : MonoBehaviour
+{
+    public Transform player;
+    //reference for my waypoints
+    public List<Transform> points;
+    //the int value for my indexed list
+    public int nextId;
+    //declare a int to help us change our nextid
+    private int idChangeValue = 1;
+    //sets our speed of the emeny
+    public float speed = 2;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform != null && player != null)
+        {
+            if (Vector2.Distance(transform.position, player.position) < 10f)
+            {
+                transform.position = Vector2.MoveTowards(transform.position, player.position, speed * Time.deltaTime);
+            }
+            else
+            {
+                MoveToNextPoint();
+            }
+        }
+    }
+
+    void MoveToNextPoint()
+    {
+        // declare and set a transform to our next point
+        Transform goalPoint = points[nextId];
+        //Flip the emeny via the transform to look at the points direction
+        //Might need to change based off of the sprites natural face
+        if (goalPoint.transform.position.x > transform.position.x)
+        {
+            transform.localScale = new Vector3(1, 1, 1);
+        }
+        else
+        {
+            transform.localScale = new Vector3(-1, 1, 1);
+        }
+        //Move the emeny towards out point
+        transform.position = Vector2.MoveTowards(transform.position, goalPoint.position, speed * Time.deltaTime);
+        //Check the distance between the emeny and the goalPoint to trigger the next point
+        if (Vector2.Distance(transform.position, goalPoint.position) < 1f)
+        {
+            idChangeValue = -1;
+        }
+    }
+}
+
+
