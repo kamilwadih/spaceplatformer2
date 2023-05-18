3d17fdb6df38040a4fc0960990dacb9b80e8cafe
kamilwadih <720013@student.dusd.net>
2023-05-17 23:06:51 -0700

    update

--- Space Platformer/Assets/Kamil/Scripts/EnemyFollow3.cs
+++ Space Platformer/Assets/Kamil/Scripts/EnemyFollow3.cs
@@ -5,22 +5,22 @@ using UnityEngine;
 public class EnemyFollow3 : MonoBehaviour
 {
     public Transform player;
-    //reference for my waypoints
     public List<Transform> points;
-    //the int value for my indexed list
     public int nextId;
-    //declare a int to help us change our nextid
     private int idChangeValue = 1;
-    //sets our speed of the emeny
     public float speed = 2;
+    public float flipDistance = 5f;
 
     // Update is called once per frame
     void Update()
     {
         if (transform != null && player != null)
         {
-            if (Vector2.Distance(transform.position, player.position) < 5f)
+            float distanceToPlayer = Vector2.Distance(transform.position, player.position);
+
+            if (distanceToPlayer < flipDistance)
             {
+                FlipEnemy(player.position.x > transform.position.x);
                 transform.position = Vector2.MoveTowards(transform.position, player.position, speed * Time.deltaTime);
             }
             else
@@ -32,30 +32,18 @@ public class EnemyFollow3 : MonoBehaviour
 
     void MoveToNextPoint()
     {
-        // declare and set a transform to our next point
         Transform goalPoint = points[nextId];
-        //Flip the emeny via the transform to look at the points direction
-        //Might need to change based off of the sprites natural face
-        if (goalPoint.transform.position.x > transform.position.x)
-        {
-            transform.localScale = new Vector3(1, 1, 1);
-        }
-        else
-        {
-            transform.localScale = new Vector3(-1, 1, 1);
-        }
-        //Move the emeny towards out point
+        FlipEnemy(goalPoint.position.x > transform.position.x);
+
         transform.position = Vector2.MoveTowards(transform.position, goalPoint.position, speed * Time.deltaTime);
-        //Check the distance between the emeny and the goalPoint to trigger the next point
+
         if (Vector2.Distance(transform.position, goalPoint.position) < 1f)
         {
-            //check if we are at the end of the line make our change value -1
-            if (nextId == points.Count -1)
+            if (nextId == points.Count - 1)
             {
                 idChangeValue = -1;
             }
 
-            //check if we are the start of the line and make our change value 1
             if (nextId == 0)
             {
                 idChangeValue = 1;
@@ -63,6 +51,16 @@ public class EnemyFollow3 : MonoBehaviour
             nextId += idChangeValue;
         }
     }
+
+    void FlipEnemy(bool faceRight)
+    {
+        if (faceRight)
+        {
+            transform.localScale = new Vector3(1, 1, 1);
+        }
+        else
+        {
+            transform.localScale = new Vector3(-1, 1, 1);
+        }
+    }
 }
-
-
